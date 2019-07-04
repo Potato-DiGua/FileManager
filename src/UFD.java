1d35108fd0f61760cac6d3b3c0035e71278ba154
wty5201314 <32703181+wty5201314@users.noreply.github.com>
2019-07-04 18:34:18 +0800

    Update UFD.java

--- src/UFD.java
+++ src/UFD.java
@@ -9,9 +9,9 @@ public class UFD {
     {
         username=name;
     }
-    public int existFile(String filepath,String filename) {//文件是否存在
+    public int existFile(String filename) {//文件是否存在
     	for(FCB i:filelist) {
-            if(i.filepath.equals(filepath)&&i.filename.equals(filename)) {
+            if(i.filename.equals(filename)) {
                 return 1;
             }
         }
@@ -19,7 +19,7 @@ public class UFD {
     }
     public FCB createFile(String filepath){//创建文件
         for(int i=1;;i++) {
-            if(existFile(filepath,"新建文件("+i+")")==0) {
+            if(existFile("新建文件("+i+")")==0) {
                 FCB file=new FCB();
                 file.filename="新建文件("+i+")";
                 file.shuxing="a";
@@ -32,41 +32,43 @@ public class UFD {
 
 
     }
-    public void deleteFile(String filepath,String filename) {//删除文件
+    public void deleteFile(String filename) {//删除文件
 
         for(int i=0;i<filelist.size();i++) {
-            if(filelist.get(i).filepath.equals(filepath)&&filelist.get(i).filename.equals(filename)) {
+            if(filelist.get(i).filename.equals(filename)) {
+            	Disk.disk_deletefile(filelist.get(i));
             	filelist.remove(i);
-            	i--;
+            	i--;           	
+            	break;
                 //删除
             }
         }
         //文件列表删除FCB
     }
-    public int xiugaiFilewords(String filepath,String filename,String words) throws UnsupportedEncodingException {//words为修改后的内容
+    public int xiugaiFilewords(String filename,String words) throws UnsupportedEncodingException {//words为修改后的内容
         for(FCB i:filelist) {
-            if(i.filepath.equals(filepath)&&i.filename.equals(filename)) {
+            if(i.filename.equals(filename)) {
                 if(i.shuxing.equals("r")) {//只读状况下不允许写入
                     return -1;
                 }else
-                    i.length=words.length();
-                	i.write(words);;
-                return 1;
+                	Disk.disk_deletefile(i);
+                    i.write(words);
+                    return 1;
             }
         }
         return -1;
     }
-    public int xiugaiFiletype(String filepath,String filename,String type) {//修改文件类型，只读r，只写w，都行a
+    public int xiugaiFiletype(String filename,String type) {//修改文件类型，只读r，只写w，都行a
     	for(FCB i:filelist) {
-            if(i.filepath.equals(filepath)&&i.filename.equals(filename)) {
+            if(i.filename.equals(filename)) {
                i.shuxing=type;
             }
         }
         return -1;
     }
-    public String openFile(String filepath,String filename) throws UnsupportedEncodingException {//打开文件
+    public String openFile(String filename) throws UnsupportedEncodingException {//打开文件
         for(FCB i:filelist) {
-            if(i.filepath.equals(filepath)&&i.filename.equals(filename)) {
+            if(i.filename.equals(filename)) {
             	if(i.shuxing.equals("a")||i.shuxing.equals("r")) {
             		return i.read();
             	}
@@ -74,14 +76,14 @@ public class UFD {
         }
         return "";
     }
-    public int  renameFile(String filepath,String filename,String rename) {//重命名文件
+    public int  renameFile(String filename,String rename) {//重命名文件
         for(FCB i:filelist) {
-            if(i.filepath.equals(filepath)&&i.filename.equals(rename)) {
+            if(i.filename.equals(rename)) {
                 return 0;//有重名，重命名失败
             }
         }
         for(FCB i:filelist) {
-            if(i.filepath.equals(filepath)&&i.filename.equals(filename)) {
+            if(i.filename.equals(filename)) {
                 i.filename=rename;//可以重命名，执行操作
                 return 1;//成功返回1
             }
