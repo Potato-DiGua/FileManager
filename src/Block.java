c628cab3d5cb8b5e8f0efb4c32b7a75763a84b9c
wty5201314 <32703181+wty5201314@users.noreply.github.com>
2019-07-04 17:03:31 +0800

    Update Block.java

--- src/Block.java
+++ src/Block.java
@@ -1,5 +1,10 @@
 public class Block {
     public int start;
     public int length;
-    public Block nextBlock;
+    public Block nextBlock=null;
+    public Block lastBlock=null;
+    public Block(int a,int b) {
+    	start=a;
+    	length=b;
+    }
 }
